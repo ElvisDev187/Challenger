0abbdfd5cc7a7bf1c50dcd11b7969d0dd583fa9a
ElvisDev187 <cabrelelvis187@gmail.com>
2023-07-08 17:24:15 +0100

    form style ok

--- app/(other)/tournoi/create/page.tsx
+++ app/(other)/tournoi/create/page.tsx
@@ -1,22 +1,41 @@
 'use client'
 
+import { ComboBox } from '@/components/ComboBox'
+import { DatePicker } from '@/components/DatePicker'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '@/components/ui/command'
 import { Input } from '@/components/ui/input'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Slider } from '@/components/ui/slider'
+import { Switch } from '@/components/ui/switch'
 import { toast } from '@/components/ui/use-toast'
+import { VILLES } from '@/constants'
 import { useCustomToasts } from '@/hooks/use-custom-toasts'
+import { cn } from '@/lib/utils'
 import { useMutation } from '@tanstack/react-query'
 import axios, { AxiosError } from 'axios'
+import { format } from 'date-fns'
+import { Image, ImagePlus } from 'lucide-react'
+import { CalendarIcon, Check, ChevronsUpDown } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+
 
 const Page = () => {
   const router = useRouter()
   const [input, setInput] = useState<string>('')
+  const [open, setOpen] = useState(false)
+  const [Ville, setVille] = useState<string>("")
+  const [age, setAge] = useState(17)
+  const [inscription, setDateIn] = useState<Date>()
+  const [debut, setDateDeb] = useState<Date>()
+
   const { loginToast } = useCustomToasts()
 
   const { mutate: createTournoi, isLoading } = useMutation({
     mutationFn: async () => {
-     
+
     },
     onError: (err: any) => {
       if (err instanceof AxiosError) {
@@ -53,46 +72,158 @@ const Page = () => {
   })
 
   return (
-    <div className='container flex items-center h-full max-w-3xl mx-auto'>
-      <div className='relative bg-white w-full h-fit p-4 rounded-lg space-y-6'>
-        <div className='flex justify-between items-center'>
-          <h1 className='text-xl font-semibold'>Create a Community</h1>
-        </div>
-
+    <>
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-10">
+        <h1 className="text-xl text-center font-bold py-5 text-slate-900 capitalize dark:text-white">Créer un Tournoi</h1>
         <hr className='bg-red-500 h-px' />
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label className="text-slate-700 text-sm font-medium dark:text-gray-200" htmlFor="username">Nom du Tournoi</label>
+              <Input id="username" type="text" className='placeholder:text-slate-900 font-semibold' />
+            </div>
 
-        <div>
-          <p className='text-lg font-medium'>Name</p>
-          <p className='text-xs pb-2'>
-            Community names including capitalization cannot be changed.
-          </p>
-          <div className='relative'>
-            <p className='absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400'>
-              r/
-            </p>
-            <Input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className='pl-6'
-            />
+            <div>
+              <label className="text-slate-700 text-sm font-medium dark:text-gray-200" htmlFor="emailAddress">Ville</label>
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-full h-[40px] justify-between"
+                  >
+                    {Ville
+                      ? VILLES.find((framework) => framework.value === Ville)?.value
+                      : "Selectionner une ville..."}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0 max-h-[250px]">
+                  <Command className='max-h-[250px]'>
+                    <CommandInput placeholder="Chercher votre ville" />
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {VILLES.map((framework) => (
+                        <CommandItem
+                          key={framework.key}
+                          onSelect={(currentValue) => {
+                            setVille(currentValue === Ville ? "" : currentValue)
+                            setOpen(false)
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              Ville === framework.value ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                          {framework.value}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div>
+              <label className="text-slate-700 text-sm font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Date limit d'inscription</label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    role="combobox"
+                    className={cn(
+                      "w-full h-[40px] justify-start text-left font-normal",
+                      !inscription && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {inscription ? <span className="font-semibold text-slate-900">{format(inscription, "PPP")}</span> : <span>Selectioner une date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={inscription}
+                    onSelect={setDateIn}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div>
+              <label className="text-slate-700 text-sm font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Date d'ouverture</label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full h-[40px] justify-start text-left font-normal",
+                      !debut && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {debut ? <span className="font-semibold text-slate-900">{format(debut, "PPP")}</span> : <span>Selectioner une date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={debut}
+                    onSelect={setDateDeb}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Switch id="airplane-mode" />
+              <label htmlFor="airplane-mode" className='text-slate-700 text-sm font-medium dark:text-gray-200'>Airplane Mode</label>
+            </div>
+            <div>
+              <label className="text-slate-700 text-sm mb-2 font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Age max : <span className="font-semibold text-slate-900">{age}</span></label>
+              <Slider defaultValue={[17]} onValueChange={(e)=> setAge(e[0])} max={100} step={1}/> 
+            </div>
+            <div>
+              <label className="text-slate-700 text-sm font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Text Area</label>
+              <textarea id="textarea" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-slate-700">
+                Image
+              </label>
+              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 bg-indigo-600 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
+                  <ImagePlus className='text-white self-center h-10 w-10' color='white'/>
+                  <div className="flex text-sm text-gray-600">
+                    <label htmlFor="file-upload" className="relative p-1 cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                      <span className="">Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                    </label>
+                    <p className="pl-1 text-white">or drag and drop</p>
+                  </div>
+                  <p className="text-xs text-white">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className='flex justify-end gap-4'>
-          <Button
-            disabled={isLoading}
-            variant='ghost'
-            onClick={() => router.back()}>
-            Cancel
-          </Button>
-          <Button
-            disabled={input.length === 0}
-            onClick={() => createTournoi()}>
-            Create Community
-          </Button>
-        </div>
-      </div>
-    </div>
+          <div className="flex justify-end mt-6 gap-4">
+            <Button
+              disabled={isLoading}
+              variant='ghost'
+              onClick={() => router.back()}>
+              Cancel
+            </Button>
+            <Button className="px-6 py-2 leading-5 ">Save</Button>
+          </div>
+        </form>
+      </section>
+
+    </>
   )
 }
 
---
+++ constants/index.ts
@@ -0,0 +1,486 @@
+export const VILLES = [
+    {
+      "value": "Garoua Boulaï",
+      "key": "GAROUA BOULAI|CM-ES"
+    },
+    {
+      "value": "Ntui",
+      "key": "NTUI|CM-CE"
+    },
+    {
+      "value": "Ndikiniméki",
+      "key": "NDIKINIMEKI|CM-CE"
+    },
+    {
+      "value": "Fontem",
+      "key": "FONTEM|CM-SW"
+    },
+    {
+      "value": "Pitoa",
+      "key": "PITOA|CM-NO"
+    },
+    {
+      "value": "Bazou",
+      "key": "BAZOU|CM-OU"
+    },
+    {
+      "value": "Ombésa",
+      "key": "OMBESA|CM-CE"
+    },
+    {
+      "value": "Mokolo",
+      "key": "MOKOLO|CM-EN"
+    },
+    {
+      "value": "Bali",
+      "key": "BALI|CM-NW"
+    },
+    {
+      "value": "Poli",
+      "key": "POLI|CM-NO"
+    },
+    {
+      "value": "Nguti",
+      "key": "NGUTI|CM-SW"
+    },
+    {
+      "value": "Mbengwi",
+      "key": "MBENGWI|CM-NW"
+    },
+    {
+      "value": "Évodoula",
+      "key": "EVODOULA|CM-CE"
+    },
+    {
+      "value": "Dschang",
+      "key": "DSCHANG|CM-OU"
+    },
+    {
+      "value": "Bangangté",
+      "key": "BANGANGTE|CM-OU"
+    },
+    {
+      "value": "Bana",
+      "key": "BANA|CM-OU"
+    },
+    {
+      "value": "Kousséri",
+      "key": "KOUSSERI|CM-EN"
+    },
+    {
+      "value": "Akonolinga",
+      "key": "AKONOLINGA|CM-CE"
+    },
+    {
+      "value": "Akono",
+      "key": "AKONO|CM-CE"
+    },
+    {
+      "value": "Mundemba",
+      "key": "MUNDEMBA|CM-SW"
+    },
+    {
+      "value": "Babanki",
+      "key": "BABANKI|CM-NW"
+    },
+    {
+      "value": "Yagoua",
+      "key": "YAGOUA|CM-EN"
+    },
+    {
+      "value": "Saa",
+      "key": "SAA|CM-CE"
+    },
+    {
+      "value": "Ngou",
+      "key": "NGOU|CM-OU"
+    },
+    {
+      "value": "Lagdo",
+      "key": "LAGDO|CM-NO"
+    },
+    {
+      "value": "Kaélé",
+      "key": "KAELE|CM-EN"
+    },
+    {
+      "value": "Ambam",
+      "key": "AMBAM|CM-SU"
+    },
+    {
+      "value": "Nkoteng",
+      "key": "NKOTENG|CM-CE"
+    },
+    {
+      "value": "Bandjoun",
+      "key": "BANDJOUN|CM-OU"
+    },
+    {
+      "value": "Nanga Eboko",
+      "key": "NANGA EBOKO|CM-CE"
+    },
+    {
+      "value": "Mbankomo",
+      "key": "MBANKOMO|CM-CE"
+    },
+    {
+      "value": "Ngoro",
+      "key": "NGORO|CM-CE"
+    },
+    {
+      "value": "Loum",
+      "key": "LOUM|CM-LT"
+    },
+    {
+      "value": "Okola",
+      "key": "OKOLA|CM-CE"
+    },
+    {
+      "value": "Bekondo",
+      "key": "BEKONDO|CM-SW"
+    },
+    {
+      "value": "Lolodorf",
+      "key": "LOLODORF|CM-SU"
+    },
+    {
+      "value": "Makary",
+      "key": "MAKARY|CM-EN"
+    },
+    {
+      "value": "Bamendjou",
+      "key": "BAMENDJOU|CM-OU"
+    },
+    {
+      "value": "Mindif",
+      "key": "MINDIF|CM-EN"
+    },
+    {
+      "value": "Mbandjok",
+      "key": "MBANDJOK|CM-CE"
+    },
+    {
+      "value": "Obala",
+      "key": "OBALA|CM-CE"
+    },
+    {
+      "value": "Mamfe",
+      "key": "MAMFE|CM-SW"
+    },
+    {
+      "value": "Sangmélima",
+      "key": "SANGMELIMA|CM-SU"
+    },
+    {
+      "value": "Njinikom",
+      "key": "NJINIKOM|CM-NW"
+    },
+    {
+      "value": "Rey Bouba",
+      "key": "REY BOUBA|CM-NO"
+    },
+    {
+      "value": "Jakiri",
+      "key": "JAKIRI|CM-NW"
+    },
+    {
+      "value": "Foumbot",
+      "key": "FOUMBOT|CM-OU"
+    },
+    {
+      "value": "Ngomedzap",
+      "key": "NGOMEDZAP|CM-CE"
+    },
+    {
+      "value": "Penja",
+      "key": "PENJA|CM-LT"
+    },
+    {
+      "value": "Okoa",
+      "key": "OKOA|CM-CE"
+    },
+    {
+      "value": "Somié",
+      "key": "SOMIE|CM-AD"
+    },
+    {
+      "value": "Bélel",
+      "key": "BELEL|CM-AD"
+    },
+    {
+      "value": "Batibo",
+      "key": "BATIBO|CM-NW"
+    },
+    {
+      "value": "Melong",
+      "key": "MELONG|CM-LT"
+    },
+    {
+      "value": "Ngambé",
+      "key": "NGAMBE|CM-LT"
+    },
+    {
+      "value": "Mouanko",
+      "key": "MOUANKO|CM-LT"
+    },
+    {
+      "value": "Dibombari",
+      "key": "DIBOMBARI|CM-LT"
+    },
+    {
+      "value": "Bogo",
+      "key": "BOGO|CM-EN"
+    },
+    {
+      "value": "Mbanga",
+      "key": "MBANGA|CM-LT"
+    },
+    {
+      "value": "Dimako",
+      "key": "DIMAKO|CM-ES"
+    },
+    {
+      "value": "Mbang",
+      "key": "MBANG|CM-ES"
+    },
+    {
+      "value": "Bankim",
+      "key": "BANKIM|CM-AD"
+    },
+    {
+      "value": "Essé",
+      "key": "ESSE|CM-CE"
+    },
+    {
+      "value": "Manjo",
+      "key": "MANJO|CM-LT"
+    },
+    {
+      "value": "Mbouda",
+      "key": "MBOUDA|CM-OU"
+    },
+    {
+      "value": "Dizangué",
+      "key": "DIZANGUE|CM-LT"
+    },
+    {
+      "value": "Yoko",
+      "key": "YOKO|CM-CE"
+    },
+    {
+      "value": "Belo",
+      "key": "BELO|CM-NW"
+    },
+    {
+      "value": "Tcholliré",
+      "key": "TCHOLLIRE|CM-NO"
+    },
+    {
+      "value": "Djohong",
+      "key": "DJOHONG|CM-AD"
+    },
+    {
+      "value": "Mvangué",
+      "key": "MVANGUE|CM-SU"
+    },
+    {
+      "value": "Banyo",
+      "key": "BANYO|CM-AD"
+    },
+    {
+      "value": "Minta",
+      "key": "MINTA|CM-CE"
+    },
+    {
+      "value": "Muyuka",
+      "key": "MUYUKA|CM-SW"
+    },
+    {
+      "value": "Fundong",
+      "key": "FUNDONG|CM-NW"
+    },
+    {
+      "value": "Yabassi",
+      "key": "YABASSI|CM-LT"
+    },
+    {
+      "value": "Bansoa",
+      "key": "BANSOA|CM-OU"
+    },
+    {
+      "value": "Mora",
+      "key": "MORA|CM-EN"
+    },
+    {
+      "value": "Diang",
+      "key": "DIANG|CM-LT"
+    },
+    {
+      "value": "Bétaré Oya",
+      "key": "BETARE OYA|CM-ES"
+    },
+    {
+      "value": "Tonga",
+      "key": "TONGA|CM-OU"
+    },
+    {
+      "value": "Bamusso",
+      "key": "BAMUSSO|CM-SW"
+    },
+    {
+      "value": "Tignère",
+      "key": "TIGNERE|CM-AD"
+    },
+    {
+      "value": "Mme-Bafumen",
+      "key": "MME-BAFUMEN|CM-NW"
+    },
+    {
+      "value": "Koza",
+      "key": "KOZA|CM-EN"
+    },
+    {
+      "value": "Ndelele",
+      "key": "NDELELE|CM-ES"
+    },
+    {
+      "value": "Yokadouma",
+      "key": "YOKADOUMA|CM-ES"
+    },
+    {
+      "value": "Tiko",
+      "key": "TIKO|CM-SW"
+    },
+    {
+      "value": "Akom II",
+      "key": "AKOM II|CM-SU"
+    },
+    {
+      "value": "Mutengene",
+      "key": "MUTENGENE|CM-SW"
+    },
+    {
+      "value": "Ndom",
+      "key": "NDOM|CM-LT"
+    },
+    {
+      "value": "Doumé",
+      "key": "DOUME|CM-ES"
+    },
+    {
+      "value": "Abong-Mbang",
+      "key": "ABONG MBANG|CM-ES"
+    },
+    {
+      "value": "Bafang",
+      "key": "BAFANG|CM-OU"
+    },
+    {
+      "value": "Bafia",
+      "key": "BAFIA|CM-CE"
+    },
+    {
+      "value": "Bafoussam",
+      "key": "BAFOUSSAM|CM-OU"
+    },
+    {
+      "value": "Bamenda",
+      "key": "BAMENDA|CM-NW"
+    },
+    {
+      "value": "Batouri",
+      "key": "BATOURI|CM-ES"
+    },
+    {
+      "value": "Bertoua",
+      "key": "BERTOUA|CM-ES"
+    },
+    {
+      "value": "Buéa",
+      "key": "BUEA|CM-SW"
+    },
+    {
+      "value": "Bélabo",
+      "key": "BELABO|CM-ES"
+    },
+    {
+      "value": "Douala",
+      "key": "DOUALA|CM-LT"
+    },
+    {
+      "value": "Ebolowa",
+      "key": "EBOLOWA|CM-SU"
+    },
+    {
+      "value": "Eséka",
+      "key": "ESEKA|CM-CE"
+    },
+    {
+      "value": "Foumban",
+      "key": "FOUMBAN|CM-OU"
+    },
+    {
+      "value": "Garoua",
+      "key": "GAROUA|CM-NO"
+    },
+    {
+      "value": "Guider",
+      "key": "GUIDER|CM-NO"
+    },
+    {
+      "value": "Kontcha",
+      "key": "KONTCHA|CM-AD"
+    },
+    {
+      "value": "Kribi",
+      "key": "KRIBI|CM-SU"
+    },
+    {
+      "value": "Kumba",
+      "key": "KUMBA|CM-SW"
+    },
+    {
+      "value": "Kumbo",
+      "key": "KUMBO|CM-NW"
+    },
+    {
+      "value": "Limbé",
+      "key": "LIMBE|CM-SW"
+    },
+    {
+      "value": "Maroua",
+      "key": "MAROUA|CM-EN"
+    },
+    {
+      "value": "Mbalmayo",
+      "key": "MBALMAYO|CM-CE"
+    },
+    {
+      "value": "Meiganga",
+      "key": "MEIGANGA|CM-AD"
+    },
+    {
+      "value": "Ngaoundéré",
+      "key": "NGAOUNDERE|CM-AD"
+    },
+    {
+      "value": "Nkongsamba",
+      "key": "NKONGSAMBA|CM-LT"
+    },
+    {
+      "value": "Tibati",
+      "key": "TIBATI|CM-AD"
+    },
+    {
+      "value": "Wum",
+      "key": "WUM|CM-NW"
+    },
+    {
+      "value": "Yaoundé",
+      "key": "YAOUNDE|CM-CE"
+    },
+    {
+      "value": "Édéa",
+      "key": "EDEA|CM-LT"
+    }
+  ]
